dfbf7d9888770953a055ee48174e7df258a17147
Victoria Kachanovska <kach.vikt@gmail.com>
2017-01-27 14:07:35 +0200

    push week 3

---
+++ src/webserverlogs/WebServerLogs.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webserverlogs;
+
+/**
+ *
+ * @author New
+ */
+public class WebServerLogs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
